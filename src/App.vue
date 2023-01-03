cb9289ebe46c89d9754cc8761520cad5b11140ab
Htun Htun Naing <htunnaing@hhn.local>
2023-01-03 14:13:45 +0630

    fixed

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-<<<<<<< HEAD
         <RouterLink to="/content">Content</RouterLink>
         <RouterLink to="/content">Blog</RouterLink>
 
-=======
->>>>>>> parent of f735b73 (add content page)
       </nav>
     </div>
   </header>
