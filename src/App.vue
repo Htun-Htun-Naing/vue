68c009af06ce87f31a449f40ea85c33451e07a52
Htun Htun Naing <htunnaing@hhn.local>
2023-01-03 11:44:56 +0630

    pth update

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="သင်လုပ်နိုင်ခဲ့ပြီ!" />
+      <HelloWorld msg="ဖြိုးသီဟ စောက်ချောကြီး!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
